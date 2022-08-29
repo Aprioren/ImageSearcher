3266d8aab042cd2339ee29c44e0bd3a417c85f40
Артем Саранча <98779874+Aprioren@users.noreply.github.com>
2022-08-29 13:37:36 +0300

    fix a brain bug with fetch query

--- src/js/fetchAPI/fetchQuery.js
+++ src/js/fetchAPI/fetchQuery.js
@@ -6,7 +6,7 @@ const KEY = 'key=29495659-8f5845388f8e68d2893953910';
 
 async function picturueQuery(query,page) {
   try {
-    const response = await axios.get(`/?${KEY}&q=${query}&${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`);
+    const response = await axios.get(`/?${KEY}&q=${query}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`);
     return response;
   } catch (error) {
     console.error(error);
